44f73002301190e293b623ac973506aff0c3cf21
Bob Gregor <bob@bobscomputers.com>
2012-05-01 21:22:27 -0400

    added short url index

--- index.php
+++ index.php
@@ -15,12 +15,35 @@ noCacheHeaders();
 <div id="wrapper">
 <a href="http://mycircletree.com" title="Circle Tree Orlando Web Design Logo"><span id="logo"></span></a>
 <?php if (AUTH) { ?>
+<h1>PHP URL Shortener</h1>
+<h2>Create New Short URL</h2>
 <form method="post" action="shorten.php" id="shortener">
 	<label for="longurl">URL to shorten</label> 
-	<input type="text" name="longurl" id="longurl" size="40" value="http://"> 
+	<input type="text" name="longurl" id="longurl" size="60" value="http://"> 
 	<input type="submit" value="Shorten">
 	<div class="error" style="display:none"></div>
 </form>
+<h2>Current Short URLS</h2>
+	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<th>Redirection</th>
+		<th>Referrals</th>
+	</tr>
+	<?php 
+	$result = mysql_query('SELECT * FROM `shortenedurls` ORDER BY `created`');
+	while ($row = mysql_fetch_assoc($result)) {
+		echo '<tr >';
+			echo '<td align="left" style="border-bottom:1px solid #000;">';
+				echo ' <input onclick="$(this).select();" style="cursor:pointer;" type="text" size="30" name="short['.$row['id'].'" value="'.BASE_HREF.getShortenedURLFromID($row['id']).'"/>&rarr; ';
+				echo $row['long_url'];
+			echo '</td>';
+			echo '<td style="border-bottom:1px solid #000;">';
+				echo $row['referrals'];
+			echo '</td>';
+		echo '</tr>';
+	}
+	?>
+	</table>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="text/javascript">
 new function($) {
